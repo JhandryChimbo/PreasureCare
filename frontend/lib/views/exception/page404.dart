70d580fa367b631d36f2d983f5bc649a0ff70852
Jhandry Chimbo <jhandry.chimbo@unl.edu.ec>
2024-11-28 00:12:06 -0500

    Mejorar página 404 con mensaje personalizado y botón de navegación a inicio

--- frontend/lib/views/exception/page404.dart
+++ frontend/lib/views/exception/page404.dart
@@ -7,15 +7,44 @@ class Page404 extends StatefulWidget {
   _Page404State createState() => _Page404State();
 }
 
-class _Page404State extends State<Page404>{
+class _Page404State extends State<Page404> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('404'),
+        title: const Text('Página No Encontrada'),
       ),
-      body: const Center(
-        child: Text('404'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+              '404',
+              style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+              '¡Vaya! La página que buscas no existe.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/');
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: const Color(0xFF2897FF),
+              ),
+              icon: const Icon(Icons.arrow_back),
+              label: const Text('Ir a Inicio'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
