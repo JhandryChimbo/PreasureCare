d7518ee46ddc88bc21cc368f2751e41ce6bdadaf
Jhandry Chimbo <jhandry.chimbo@unl.edu.ec>
2024-12-05 23:51:42 -0500

    Agregar método de registro en FacadeServices para manejar la creación de usuarios

--- frontend/lib/controls/backendService/FacadeServices.dart
+++ frontend/lib/controls/backendService/FacadeServices.dart
@@ -31,4 +31,30 @@ class FacadeServices {
 
     return loginResponse;
   }
+
+  Future<Login> register(Map<String, String> credentials) async {
+    const Map<String, String> headers = {"Content-Type": "application/json"};
+    const String url = '${Conexion.URL}persona/save';
+    final uri = Uri.parse(url);
+    Login loginResponse = Login(msg: '', code: 0, data: {});
+
+    try {
+      final response = await http.post(uri, headers: headers, body: jsonEncode(credentials));
+      final Map<String, dynamic> responseBody = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        loginResponse = Login(msg: responseBody['msg'], code: responseBody['code'], data: responseBody['data']);
+      } else {
+        loginResponse = Login(
+          msg: responseBody.containsKey('msg') ? responseBody['msg'] : "Error desconocido",
+          code: response.statusCode,
+          data: responseBody['data'] ?? {},
+        );
+      }
+    } catch (e) {
+      loginResponse = Login(msg: "Error Inesperado", code: 500, data: {});
+    }
+
+    return loginResponse;
+  }
 }
