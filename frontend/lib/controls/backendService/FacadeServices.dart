ef23ffbce967c96d1bc8df1645865be48e60f63d
Jhandry Chimbo <jhandry.chimbo@unl.edu.ec>
2024-12-06 23:02:08 -0500

    Implementar registro de usuario con nuevos campos y manejo de respuestas

--- frontend/lib/controls/backendService/FacadeServices.dart
+++ frontend/lib/controls/backendService/FacadeServices.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:frontend/controls/backendService/model/register.dart';
 import 'package:frontend/controls/conexion.dart';
 import 'package:frontend/controls/backendService/model/login.dart';
 import 'package:http/http.dart' as http;
@@ -32,29 +33,29 @@ class FacadeServices {
     return loginResponse;
   }
 
-  Future<Login> register(Map<String, String> credentials) async {
+  Future<Register> register(Map<String, String> credentials) async {
     const Map<String, String> headers = {"Content-Type": "application/json"};
     const String url = '${Conexion.URL}persona/save';
     final uri = Uri.parse(url);
-    Login loginResponse = Login(msg: '', code: 0, data: {});
+    Register registerResponse = Register(msg: '', code: 0, data: {});
 
     try {
       final response = await http.post(uri, headers: headers, body: jsonEncode(credentials));
       final Map<String, dynamic> responseBody = jsonDecode(response.body);
 
       if (response.statusCode == 200) {
-        loginResponse = Login(msg: responseBody['msg'], code: responseBody['code'], data: responseBody['data']);
+        registerResponse = Register(msg: responseBody['msg'], code: responseBody['code'], data: responseBody['data']);
       } else {
-        loginResponse = Login(
+        registerResponse = Register(
           msg: responseBody.containsKey('msg') ? responseBody['msg'] : "Error desconocido",
           code: response.statusCode,
           data: responseBody['data'] ?? {},
         );
       }
     } catch (e) {
-      loginResponse = Login(msg: "Error Inesperado", code: 500, data: {});
+      registerResponse = Register(msg: "Error Inesperado", code: 500, data: {});
     }
 
-    return loginResponse;
+    return registerResponse;
   }
 }
