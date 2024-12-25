ff90a5da286cfeeec578f852a9d223a023357580
Jhandry Chimbo <jhandry.chimbo@unl.edu.ec>
2024-12-24 23:18:48 -0500

    Refactorizar el servicio de pacientes para mejorar la gestión de errores y actualizar la vista de lista de pacientes para incluir un botón de recarga.

--- frontend/lib/services/patient_service.dart
+++ frontend/lib/services/patient_service.dart
@@ -1,28 +1,21 @@
-import 'dart:developer';
 import 'package:frontend/controls/backendService/facade_services.dart';
-import 'package:frontend/controls/backendService/generic_answer.dart';
 import 'package:frontend/widgets/toast/error.dart';
 
 class PatientService {
   static Future<void> listPatients() async {
     try {
       final facadeServices = FacadeServices();
-      final GenericAnswer listado = await facadeServices.listarPacientes();
-
-      if (listado.data is List) {
-        final patients = listado.data as List<dynamic>;
-
-        // Imprimir cada paciente de manera ordenada
-        for (var patient in patients) {
-          '${patient['nombre']} ${patient['apellido']}';
-        }
+      final listado = await facadeServices.listarPacientes();
+      if (listado.code == 200) {
+        final patients = listado.data;
+        print(patients);
       } else {
         ErrorToast.show("No se encontró información de pacientes.");
-        log("Listado vacío o no es una lista.");
+        print("Listado vacío o no es una lista.");
       }
     } catch (error) {
       ErrorToast.show("Error al listar pacientes: $error");
-      log("Error: $error");
+      print(error);
     }
   }
 }
