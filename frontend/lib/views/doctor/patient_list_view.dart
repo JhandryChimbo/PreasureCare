39d4abf826db73e2cf06eabce27d257d4d7526c4
Jhandry Chimbo <jhandry.chimbo@unl.edu.ec>
2024-12-26 22:09:10 -0500

    Refactorizar la vista de lista de pacientes para utilizar el nuevo servicio de facade y mejorar la gestión de estado. Eliminar el servicio de pacientes obsoleto.

--- frontend/lib/views/doctor/patient_list_view.dart
+++ frontend/lib/views/doctor/patient_list_view.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/services/patient_service.dart';
-import 'package:frontend/widgets/buttons/button.dart';
+import 'package:frontend/controls/backendService/facade_services.dart';
 
-class PatientListView extends StatelessWidget {
+class PatientListView extends StatefulWidget {
   const PatientListView({super.key});
 
   @override
+  _PatientListViewState createState() => _PatientListViewState();
+}
+
+class _PatientListViewState extends State<PatientListView> {
+  List<Map<String, dynamic>> patients = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _listarPatients();
+  }
+
+  Future<void> _listarPatients() async {
+    try {
+      FacadeServices services = FacadeServices();
+      var response = await services.listarPacientes();
+
+      if (response.code == 200) {
+        setState(() {
+          patients = List<Map<String, dynamic>>.from(response.data);
+        });
+      } else {
+        print('Error al listar pacientes');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    PatientService.listPatients();
     return Scaffold(
-      appBar: AppBar(title: const Text('Lista de Pacientes')),
+      appBar: AppBar(
+        title: const Text('Lista de pacientes'),
+      ),
       body: Column(
         children: [
-          ConfirmButton(text: "Recargar", onPressed: () => PatientService.listPatients()),
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
+              itemCount: patients.length,
               itemBuilder: (context, index) {
-                return const ListTile(
-                  title: Text("Paciente xd"),
+                final patient = patients[index];
+                return Card(
+                  margin: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title:
+                        Text('${patient['nombres']} ${patient['apellidos']}'),
+                    subtitle: Text('Correo: ${patient['cuenta']['correo']}'),
+                  ),
                 );
               },
             ),
@@ -28,4 +63,3 @@ class PatientListView extends StatelessWidget {
     );
   }
 }
-
